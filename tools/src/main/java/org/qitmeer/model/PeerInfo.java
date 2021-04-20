c7c9a0065edb545b52e93964e738c6d66855ea05
noctis0227 <lnoctis@hotmail.com>
2021-04-20 14:26:03 +0800

    modify PeerInfo model

--- tools/src/main/java/org/qitmeer/model/PeerInfo.java
+++ tools/src/main/java/org/qitmeer/model/PeerInfo.java
@@ -1,219 +1,29 @@
 package org.qitmeer.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PeerInfo {
-    private String uuid;
-    private int id;
-    private String addr;
-    private String addrlocal;
-    private String services;
-    private boolean relaytxes;
-    private long lastsend;
-    private long lastrecv;
-    private int bytessent;
-    private int bytesrecv;
-    private long conntime;
-    private int timeoffset;
-    private int pingtime;
-    private int version;
-    private String subver;
-    private boolean inbound;
-    private int banscore;
-    private boolean syncnode;
-    private Graphstate graphstate;
+    public String address;
+    public long timeoffset;
+    public long bytesrecv;
+    public String useragent;
+    public String services;
+    public String lastrecv;
+    public String lastsend;
+    public String genesis;
+    public long protocol;
+    public long bytessent;
+    public String conntime;
+    public String id;
+    public String state;
+    public String direction;
+    public GraphState graphstate;
 
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setAddr(String addr) {
-        this.addr = addr;
-    }
-
-    public String getAddr() {
-        return addr;
-    }
-
-    public void setAddrlocal(String addrlocal) {
-        this.addrlocal = addrlocal;
-    }
-
-    public String getAddrlocal() {
-        return addrlocal;
-    }
-
-    public void setServices(String services) {
-        this.services = services;
-    }
-
-    public String getServices() {
-        return services;
-    }
-
-    public void setRelaytxes(boolean relaytxes) {
-        this.relaytxes = relaytxes;
-    }
-
-    public boolean getRelaytxes() {
-        return relaytxes;
-    }
-
-    public void setLastsend(long lastsend) {
-        this.lastsend = lastsend;
-    }
-
-    public long getLastsend() {
-        return lastsend;
-    }
-
-    public void setLastrecv(long lastrecv) {
-        this.lastrecv = lastrecv;
-    }
-
-    public long getLastrecv() {
-        return lastrecv;
-    }
-
-    public void setBytessent(int bytessent) {
-        this.bytessent = bytessent;
-    }
-
-    public int getBytessent() {
-        return bytessent;
-    }
-
-    public void setBytesrecv(int bytesrecv) {
-        this.bytesrecv = bytesrecv;
-    }
-
-    public int getBytesrecv() {
-        return bytesrecv;
-    }
-
-    public void setConntime(long conntime) {
-        this.conntime = conntime;
-    }
-
-    public long getConntime() {
-        return conntime;
-    }
-
-    public void setTimeoffset(int timeoffset) {
-        this.timeoffset = timeoffset;
-    }
-
-    public int getTimeoffset() {
-        return timeoffset;
-    }
-
-    public void setPingtime(int pingtime) {
-        this.pingtime = pingtime;
-    }
-
-    public int getPingtime() {
-        return pingtime;
-    }
-
-    public void setVersion(int version) {
-        this.version = version;
-    }
-
-    public int getVersion() {
-        return version;
-    }
-
-    public void setSubver(String subver) {
-        this.subver = subver;
-    }
-
-    public String getSubver() {
-        return subver;
-    }
-
-    public void setInbound(boolean inbound) {
-        this.inbound = inbound;
-    }
-
-    public boolean getInbound() {
-        return inbound;
-    }
-
-    public void setBanscore(int banscore) {
-        this.banscore = banscore;
-    }
-
-    public int getBanscore() {
-        return banscore;
-    }
-
-    public void setSyncnode(boolean syncnode) {
-        this.syncnode = syncnode;
-    }
-
-    public boolean getSyncnode() {
-        return syncnode;
-    }
-
-    public void setGraphstate(Graphstate graphstate) {
-        this.graphstate = graphstate;
-    }
-
-    public Graphstate getGraphstate() {
-        return graphstate;
-    }
-
-
-    public class Graphstate {
-
-        private List<String> tips;
-        private int mainorder;
-        private int mainheight;
-        private int layer;
-
-        public void setTips(List<String> tips) {
-            this.tips = tips;
-        }
-
-        public List<String> getTips() {
-            return tips;
-        }
-
-        public void setMainorder(int mainorder) {
-            this.mainorder = mainorder;
-        }
-
-        public int getMainorder() {
-            return mainorder;
-        }
-
-        public void setMainheight(int mainheight) {
-            this.mainheight = mainheight;
-        }
-
-        public int getMainheight() {
-            return mainheight;
-        }
-
-        public void setLayer(int layer) {
-            this.layer = layer;
-        }
-
-        public int getLayer() {
-            return layer;
-        }
-
+    public class GraphState {
+        public long mainheight;
+        public long layer;
+        public long mainorder;
+        public ArrayList<String> tips;
     }
 }
--- tools/src/test/java/org/qitmeer/util/QitmeerTest.java
+++ tools/src/test/java/org/qitmeer/util/QitmeerTest.java
@@ -1,8 +1,8 @@
 package org.qitmeer.util;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 import org.qitmeer.model.*;
 
@@ -10,165 +10,177 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QitmeerTest {
-    Qitmeer q = new Qitmeer("http://127.0.0.1:8131","admin","123456");
+    Qitmeer q = new Qitmeer("http://127.0.0.1", "admin", "123");
 
     @Test
-    public void getBlockCountTest(){
-      ServiceResult result =  q.getBlockCount();
-        if (result.code==0){
-            System.out.println("getBlockCountTest:"+result.data.toString());
-        }else{
-            System.out.println("getBlockCountTest:"+result.msg);
+    public void getBlockCountTest() {
+        ServiceResult result = q.getBlockCount();
+        if (result.code == 0) {
+            System.out.println("getBlockCountTest:\r\n" + result.data.toString());
+        } else {
+            System.out.println("getBlockCountTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getBlockByOrderTest(){
-      ServiceResult result =  q.getBlockByOrder(1700,true);
-        if (result.code==0){
+    public void getBlockByOrderTest() {
+        ServiceResult result = q.getBlockByOrder(1700, true);
+        if (result.code == 0) {
             JSONObject recJson = JSON.parseObject(result.data.toString());
             Block b = JSON.toJavaObject(recJson, Block.class);
-            System.out.println("getBlockByOrderTest:"+b.getHash());
-        }else{
-            System.out.println("getBlockByOrderTest:"+result.msg);
+            System.out.println("getBlockByOrderTest:\r\n" + b.getHash());
+        } else {
+            System.out.println("getBlockByOrderTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getBlockHeaderTest(){
-      ServiceResult result =  q.getBlockHeader("0961060de66cd2bb62559a5eee5a12cf648df09ca727800daa48c71d99756e85",true);
-        if (result.code==0){
+    public void getBlockHeaderTest() {
+        ServiceResult result = q.getBlockHeader("61f90fa6286d2ad8290260775aeb07e2f70b1d8184d9ded5f852983dadd25c94", true);
+        if (result.code == 0) {
             JSONObject recJson = JSON.parseObject(result.data.toString());
-            BlockHeader b =  JSON.toJavaObject(recJson, BlockHeader.class);
-            System.out.println("getBlockHeaderTest:"+b.getHash());
-        }else{
-            System.out.println("getBlockHeaderTest:"+result.msg);
+            BlockHeader b = JSON.toJavaObject(recJson, BlockHeader.class);
+            System.out.println("getBlockHeaderTest:\r\n" + b.getHash());
+        } else {
+            System.out.println("getBlockHeaderTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getBlockWeightTest(){
-      ServiceResult result =  q.getBlockWeight("0961060de66cd2bb62559a5eee5a12cf648df09ca727800daa48c71d99756e85");
-        if (result.code==0){
-            System.out.println("getBlockWeightTest:"+result.data.toString());
-        }else{
-            System.out.println("getBlockWeightTest:"+result.msg);
+    public void getBlockWeightTest() {
+        ServiceResult result = q.getBlockWeight("61f90fa6286d2ad8290260775aeb07e2f70b1d8184d9ded5f852983dadd25c94");
+        if (result.code == 0) {
+            System.out.println("getBlockWeightTest:\r\n" + result.data.toString());
+        } else {
+            System.out.println("getBlockWeightTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getBlockByhashTest(){
-      ServiceResult result =  q.getBlockByhash("0961060de66cd2bb62559a5eee5a12cf648df09ca727800daa48c71d99756e85",true);
-        if (result.code==0){
+    public void getBlockByhashTest() {
+        ServiceResult result = q.getBlockByhash("61f90fa6286d2ad8290260775aeb07e2f70b1d8184d9ded5f852983dadd25c94", true);
+        if (result.code == 0) {
             JSONObject recJson = JSON.parseObject(result.data.toString());
-            Block b =  JSON.toJavaObject(recJson,Block.class);
-            System.out.println("getBlockByhashTest:"+b.getHash());
-        }else{
-            System.out.println("getBlockByhashTest:"+result.msg);
+            Block b = JSON.toJavaObject(recJson, Block.class);
+            System.out.println("getBlockByhashTest:\r\n" + b.getHash());
+        } else {
+            System.out.println("getBlockByhashTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getBlockHashTest(){
-      ServiceResult result =  q.getBlockHash(1700);
-        if (result.code==0){
-            System.out.println("getBlockHashTest:"+result.data.toString());
-        }else{
-            System.out.println("getBlockHashTest:"+result.msg);
+    public void getBlockHashTest() {
+        ServiceResult result = q.getBlockHash(1700);
+        if (result.code == 0) {
+            System.out.println("getBlockHashTest:\r\n" + result.data.toString());
+        } else {
+            System.out.println("getBlockHashTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getBestBlockHashTest(){
-      ServiceResult result =  q.getBestBlockHash();
-        if (result.code==0){
-            System.out.println("getBestBlockHashTest:"+result.data.toString());
-        }else{
-            System.out.println("getBestBlockHashTest:"+result.msg);
+    public void getBestBlockHashTest() {
+        ServiceResult result = q.getBestBlockHash();
+        if (result.code == 0) {
+            System.out.println("getBestBlockHashTest:\r\n" + result.data.toString());
+        } else {
+            System.out.println("getBestBlockHashTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void isOnMainChain(){
-      ServiceResult result =  q.isOnMainChain("0961060de66cd2bb62559a5eee5a12cf648df09ca727800daa48c71d99756e85");
-        if (result.code==0){
-            System.out.println("isOnMainChain:"+result.data.toString());
-        }else{
-            System.out.println("isOnMainChain:"+result.msg);
+    public void isOnMainChain() {
+        ServiceResult result = q.isOnMainChain("61f90fa6286d2ad8290260775aeb07e2f70b1d8184d9ded5f852983dadd25c94");
+        if (result.code == 0) {
+            System.out.println("isOnMainChain:\r\n" + result.data.toString());
+        } else {
+            System.out.println("isOnMainChain:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getPeerInfoTest(){
-      ServiceResult result =  q.getPeerInfo();
-        if (result.code==0){
-            List list = JSON.parseArray(result.data.toString(),PeerInfo.class);
-            System.out.println("getPeerInfoTest:"+list.size());
-        }else{
-            System.out.println("getPeerInfoTest:"+result.msg);
+    public void getPeerInfoTest() {
+        ServiceResult result = q.getPeerInfo();
+        if (result.code == 0) {
+            List<PeerInfo> list = JSON.parseArray(result.data.toString(), PeerInfo.class);
+            System.out.println("getPeerInfoTest:\r\n" + list.size());
+        } else {
+            System.out.println("getPeerInfoTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getNodeInfoTest(){
-      ServiceResult result =  q.getNodeInfo();
-        if (result.code==0){
+    public void getNodeInfoTest() {
+        ServiceResult result = q.getNodeInfo();
+        if (result.code == 0) {
             JSONObject recJson = JSON.parseObject(result.data.toString());
-            NodeInfo b =  JSON.toJavaObject(recJson, NodeInfo.class);
-            System.out.println("getNodeInfoTest:"+b.getUUID());
-        }else{
-            System.out.println("getNodeInfoTest:"+result.msg);
+            NodeInfo b = JSON.toJavaObject(recJson, NodeInfo.class);
+            System.out.println("getNodeInfoTest:\r\n" + b.getUUID());
+        } else {
+            System.out.println("getNodeInfoTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getUtxoTest(){
-      ServiceResult result =  q.getUtxo("019edb774482a9f619ca02f949881c2c9616a48e014ad27668ee5e56b64182f9",0,true);
-        if (result.code==0){
+    public void getUtxoTest() {
+        ServiceResult result = q.getUtxo("243907bffb97da381c98a095cb758c46f720980511f2df8923e93a64258cfb1e", 0, true);
+        if (result.code == 0) {
             JSONObject recJson = JSON.parseObject(result.data.toString());
-            Utxo b= JSON.toJavaObject(recJson,Utxo.class);
-            System.out.println("getUtxoTest:"+b.getCoinbase());
-        }else{
-            System.out.println("getUtxoTest:"+result.msg);
+            Utxo b = JSON.toJavaObject(recJson, Utxo.class);
+            System.out.println("getUtxoTest:\r\n" + b.getCoinbase());
+        } else {
+            System.out.println("getUtxoTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void createRawTransactionTest(){
+    public void createRawTransactionTest() {
         FromTx u = new FromTx();
         u.setTxid("019edb774482a9f619ca02f949881c2c9616a48e014ad27668ee5e56b64182f9");
         u.setVout(0);
         List<FromTx> list = new ArrayList<>();
         list.add(u);
-      ServiceResult result =  q.createRawTransaction(list,"TmgD1mu8zMMV9aWmJrXqQYnWRhR9SBfDZG6",120,0);
-        if (result.code==0){
-            System.out.println("createRawTransactionTest:"+result.data.toString());
-        }else{
-            System.out.println("createRawTransactionTest:"+result.msg);
+        ServiceResult result = q.createRawTransaction(list, "TmgD1mu8zMMV9aWmJrXqQYnWRhR9SBfDZG6", 120, 0);
+        if (result.code == 0) {
+            System.out.println("createRawTransactionTest:\r\n" + result.data.toString());
+        } else {
+            System.out.println("createRawTransactionTest:\r\n" + result.msg);
         }
     }
 
 
-
     @Test
-    public void decodeRawTransactionTest(){
-        ServiceResult result =  q.decodeRawTransaction("0100000001f98241b6565eee6876d24a018ea416962c1c8849f902ca19f6a9824477db9e0100000000ffffffff0178000000000000001976a914bd4d1888cb054b2755d65d93c356573e4d283ead88ac00000000000000000100");
-        if (result.code==0){
-            System.out.println("decodeRawTransactionTest:"+result.data.toString());
-        }else{
-            System.out.println("decodeRawTransactionTest:"+result.msg);
+    public void decodeRawTransactionTest() {
+        ServiceResult result = q.decodeRawTransaction("0100000001f98241b6565eee6876d24a018ea416962c1c8849f902ca19f6a9824477db9e0100000000ffffffff0178000000000000001976a914bd4d1888cb054b2755d65d93c356573e4d283ead88ac00000000000000000100");
+        if (result.code == 0) {
+            System.out.println("decodeRawTransactionTest:\r\n" + result.data.toString());
+        } else {
+            System.out.println("decodeRawTransactionTest:\r\n" + result.msg);
         }
     }
+
     @Test
-    public void getRawTransactionTest(){
-        ServiceResult result =  q.getRawTransaction("f5f5c09644789d3b61c9b5f0eb7f69b594605defc9c519eba1dd3231a4cba4dc",true);
-        if (result.code==0){
+    public void getRawTransactionTest() {
+        ServiceResult result = q.getRawTransaction("243907bffb97da381c98a095cb758c46f720980511f2df8923e93a64258cfb1e", true);
+        if (result.code == 0) {
             JSONObject recJson = JSON.parseObject(result.data.toString());
-            TxRawResult b=  JSON.toJavaObject(recJson,TxRawResult.class);
-            System.out.println("getRawTransactionTest:"+b.getTxid());
-        }else{
-            System.out.println("getRawTransactionTest:"+result.msg);
+            TxRawResult b = JSON.toJavaObject(recJson, TxRawResult.class);
+            System.out.println("getRawTransactionTest:\r\n" + b.getTxid());
+        } else {
+            System.out.println("getRawTransactionTest:\r\n" + result.msg);
         }
     }
 
     @Test
-    public void sendRawTransactionTest(){
-        ServiceResult result =  q.sendRawTransaction("0100000001f98241b6565eee6876d24a018ea416962c1c8849f902ca19f6a9824477db9e0100000000ffffffff0178000000000000001976a914bd4d1888cb054b2755d65d93c356573e4d283ead88ac00000000000000000100",true);
-        if (result.code==0){
+    public void sendRawTransactionTest() {
+        ServiceResult result = q.sendRawTransaction("0100000001f98241b6565eee6876d24a018ea416962c1c8849f902ca19f6a9824477db9e0100000000ffffffff0178000000000000001976a914bd4d1888cb054b2755d65d93c356573e4d283ead88ac00000000000000000100", true);
+        if (result.code == 0) {
             JSONObject recJson = JSON.parseObject(result.data.toString());
-            Utxo b= JSON.toJavaObject(recJson,Utxo.class);
-            System.out.println("sendRawTransactionTest:"+result.data.toString());
-        }else{
-            System.out.println("sendRawTransactionTest:"+result.msg);
+            Utxo b = JSON.toJavaObject(recJson, Utxo.class);
+            System.out.println("sendRawTransactionTest:\r\n" + result.data.toString());
+
+        } else {
+            System.out.println("sendRawTransactionTest:\r\n" + result.msg);
         }
     }
 }
